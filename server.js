80e447aa3eaed822360cb45364a3cf0b932c74f7
Logan Rice <loganrice@Logans-MacBook-Air.local>
2015-04-24 14:26:39 -0700

    done but with errors

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 
-
 var app = express()
-app.use('/api/posts', require('./controllers/api/posts'))
 app.use(bodyParser.json())
+app.use(require('./controllers'))
 
-app.get('/', function (req, res) {
-  res.sendfile('layouts/posts.html')
-})
-
-app.listen(3000, function () {
-  console.log('Server listening on', 3000)
+var server = app.listen(3000, function () {
+  console.log('Server listening on %d', server.address().port)
 })
 
